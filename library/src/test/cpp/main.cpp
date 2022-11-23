65d69d790245223ed2a337b86dcae71f041383ff
jiacheng.li <jiacheng.li@shanbay.com>
2022-11-23 16:57:21 +0800

    support read all

--- library/src/test/cpp/main.cpp
+++ library/src/test/cpp/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include "../../main/cpp/core.h"
+#include <string>
+#include <inttypes.h>
 
 int main(int argc, char *argv[]) {
     std::cout << "init" << std::endl;
@@ -12,6 +14,11 @@ int main(int argc, char *argv[]) {
     kv->write_float("float", 2);
     kv->write_int32("int32", 0x12345678);
     kv->write_int64("int64", 0x1234567878563412);
+    std::cout << "read all" << std::endl;
+    kv->read_all([=](const char *const key, const nkv::byte * value, nkv::byte type, size_t size) {
+        std::string s((char* ) value, size);
+        std::cout << "key: " << key << "| type:" << type << "| value: " << s << std::endl;
+    });
     std::cout << "destroy" << std::endl;
     nkv::KV::destroy(kv);
     return 0;
