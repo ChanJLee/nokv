59daa7df5736d2ed6d46f566ae5f0b217b34311b
jiacheng.li <jiacheng.li@shanbay.com>
2022-11-29 11:55:01 +0800

    update header size

--- library/src/test/cpp/main.cpp
+++ library/src/test/cpp/main.cpp
@@ -33,43 +33,43 @@ int main(int argc, char *argv[]) {
     nokv::KV *kv = nokv::KV::create(argv[2]);
     std::cout << "write" << std::endl;
     kv->put_string("string", "hello world");
-    kv->put_boolean("boolean", true);
-    kv->put_float("float", 2);
-    kv->put_int32("int32", 0x12345678);
-    kv->put_int64("int64", 0x1234567878563412);
-    std::cout << "read all" << std::endl;
-    kv->read_all([=](const char *const key, nokv::Entry *entry) {
-        printf("key: %s, value: ", key);
-        switch (entry->type()) {
-            case nokv::TYPE_INT64: {
-                nokv::kv_int64_t v = entry->as_int64();
-                printf("0x%llx", v);
-                break;
-            }
-            case nokv::TYPE_INT32: {
-                nokv::kv_int32_t v = entry->as_int32();
-                printf("0x%x", v);
-                break;
-            }
-            case nokv::TYPE_FLOAT: {
-                nokv::kv_float_t v = entry->as_float();
-                printf("%f", v);
-                break;
-            }
-            case nokv::TYPE_BOOLEAN: {
-                nokv::kv_boolean_t v = entry->as_boolean();
-                printf("%d", v);
-                break;
-            }
-            case nokv::TYPE_STRING: {
-                const char *v = entry->as_string().str_;
-                printf("%s", v);
-                break;
-            }
-        }
-        printf("\n");
-    });
-    std::cout << "destroy" << std::endl;
+//    kv->put_boolean("boolean", true);
+//    kv->put_float("float", 2);
+//    kv->put_int32("int32", 0x12345678);
+//    kv->put_int64("int64", 0x1234567878563412);
+//    std::cout << "read all" << std::endl;
+//    kv->read_all([=](const char *const key, nokv::Entry *entry) {
+//        printf("key: %s, value: ", key);
+//        switch (entry->type()) {
+//            case nokv::TYPE_INT64: {
+//                nokv::kv_int64_t v = entry->as_int64();
+//                printf("0x%llx", v);
+//                break;
+//            }
+//            case nokv::TYPE_INT32: {
+//                nokv::kv_int32_t v = entry->as_int32();
+//                printf("0x%x", v);
+//                break;
+//            }
+//            case nokv::TYPE_FLOAT: {
+//                nokv::kv_float_t v = entry->as_float();
+//                printf("%f", v);
+//                break;
+//            }
+//            case nokv::TYPE_BOOLEAN: {
+//                nokv::kv_boolean_t v = entry->as_boolean();
+//                printf("%d", v);
+//                break;
+//            }
+//            case nokv::TYPE_STRING: {
+//                const char *v = entry->as_string().str_;
+//                printf("%s", v);
+//                break;
+//            }
+//        }
+//        printf("\n");
+//    });
+//    std::cout << "destroy" << std::endl;
     nokv::KV::destroy(kv);
     return 0;
 }
