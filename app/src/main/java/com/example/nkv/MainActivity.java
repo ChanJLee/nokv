8cce6fb9a1ed43d67cb30cdec6ee159bf0b39aa2
jiacheng.li <jiacheng.li@shanbay.com>
2022-12-19 14:02:51 +0800

    add test code

--- app/src/main/java/com/example/nkv/MainActivity.java
+++ app/src/main/java/com/example/nkv/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.SystemClock;
 import android.util.Log;
+import android.view.View;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,54 +20,59 @@ public class MainActivity extends AppCompatActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		new Thread(new Runnable() {
+		findViewById(R.id.btn).setOnClickListener(new View.OnClickListener() {
 			@Override
-			public void run() {
-				List<String> intKeys = new ArrayList<>(10240);
-				List<String> stringKeys = new ArrayList<>(10240);
-				for (int i = 0; i < 10000; ++i) {
-					intKeys.add("key_i" + i);
-					stringKeys.add("key_s" + i);
-				}
+			public void onClick(View v) {
+				new Thread(new Runnable() {
+					@Override
+					public void run() {
+						List<String> intKeys = new ArrayList<>(10240);
+						List<String> stringKeys = new ArrayList<>(10240);
+						for (int i = 0; i < 10000; ++i) {
+							intKeys.add("key_i" + i);
+							stringKeys.add("key_s" + i);
+						}
 
-				long ts = SystemClock.elapsedRealtime();
-				SharedPreferences sp = getSharedPreferences("fuck1", MODE_PRIVATE);
-				SharedPreferences.Editor editor = sp.edit();
-				for (int i = 0; i < intKeys.size(); ++i) {
-					editor.putInt(intKeys.get(i), i);
-					editor.putString(stringKeys.get(i), stringKeys.get(i));
-				}
-				editor.commit();
-				long ts2 = SystemClock.elapsedRealtime();
-				Log.d("NoKV", "sp write: " + (ts2 - ts));
+						long ts = SystemClock.elapsedRealtime();
+						SharedPreferences sp = getSharedPreferences("fuck1", MODE_PRIVATE);
+						SharedPreferences.Editor editor = sp.edit();
+						for (int i = 0; i < intKeys.size(); ++i) {
+							editor.putInt(intKeys.get(i), i);
+							editor.putString(stringKeys.get(i), stringKeys.get(i));
+						}
+						editor.commit();
+						long ts2 = SystemClock.elapsedRealtime();
+						Log.d("NoKV", "sp write: " + (ts2 - ts));
 
-				ts = ts2;
-				for (int i = 0; i < intKeys.size(); ++i) {
-					sp.getInt(intKeys.get(i), 0);
-					sp.getString(stringKeys.get(i), null);
-				}
-				ts2 = SystemClock.elapsedRealtime();
-				Log.d("NoKV", "sp read: " + (ts2 - ts));
+						ts = ts2;
+						for (int i = 0; i < intKeys.size(); ++i) {
+							sp.getInt(intKeys.get(i), 0);
+							sp.getString(stringKeys.get(i), null);
+						}
+						ts2 = SystemClock.elapsedRealtime();
+						Log.d("NoKV", "sp read: " + (ts2 - ts));
 
-				ts = ts2;
-				sp = NoKV.create("fuck1", MODE_PRIVATE);
-				editor = sp.edit();
-				for (int i = 0; i < intKeys.size(); ++i) {
-					editor.putInt(intKeys.get(i), i);
-					editor.putString(stringKeys.get(i), stringKeys.get(i));
-				}
-				editor.commit();
-				ts2 = SystemClock.elapsedRealtime();
-				Log.d("NoKV", "nokv write: " + (ts2 - ts));
+						ts = ts2;
+						sp = NoKV.create("fuck1", MODE_PRIVATE);
+						editor = sp.edit();
+						for (int i = 0; i < intKeys.size(); ++i) {
+							editor.putInt(intKeys.get(i), i);
+							editor.putString(stringKeys.get(i), stringKeys.get(i));
+						}
+						editor.commit();
+						ts2 = SystemClock.elapsedRealtime();
+						Log.d("NoKV", "nokv write: " + (ts2 - ts));
 
-				ts = ts2;
-				for (int i = 0; i < intKeys.size(); ++i) {
-					sp.getInt(intKeys.get(i), 0);
-					sp.getString(stringKeys.get(i), null);
-				}
-				ts2 = SystemClock.elapsedRealtime();
-				Log.d("NoKV", "nokv read: " + (ts2 - ts));
+						ts = ts2;
+						for (int i = 0; i < intKeys.size(); ++i) {
+							sp.getInt(intKeys.get(i), 0);
+							sp.getString(stringKeys.get(i), null);
+						}
+						ts2 = SystemClock.elapsedRealtime();
+						Log.d("NoKV", "nokv read: " + (ts2 - ts));
+					}
+				}, "FuckOff").start();
 			}
-		}, "FuckOff").start();
+		});
 	}
 }
